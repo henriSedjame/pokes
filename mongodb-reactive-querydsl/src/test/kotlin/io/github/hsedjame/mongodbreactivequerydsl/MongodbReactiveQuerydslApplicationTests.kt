f3628efd037005cad452f46ecf94500adfe9b2ae
hsedjame <sedhjodev@gmail.com>
2022-02-01 15:03:50 +0100

    add mongodb-reactive-querydsl module

---
+++ mongodb-reactive-querydsl/src/test/kotlin/io/github/hsedjame/mongodbreactivequerydsl/MongodbReactiveQuerydslApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.hsedjame.mongodbreactivequerydsl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongodbReactiveQuerydslApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
